4cc708560eaee815751eec5c9eda199eaea58c94
alexandra <alexandraschurman@gmail.com>
2022-02-26 14:26:50 -0800

    commit tree 43b9fa625d1eb6aaf9c45c206c63bbe8e5536400

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,61 @@
 # your code goes here
+require 'pry'
+class Person
+    attr_accessor :bank_account
+    attr_reader :name, :happiness, :hygeine
+
+    def initialize(name)
+        @name = name #invoking a method
+        @bank_account = 25
+        @happiness = 8
+        @hygeine = 8
+        #end of initialize
+    end
+
+
+    def name  #reader method - expose data
+        @name
+    end
+
+    def name=(name) #writer method - assigns values 
+        @name = name
+    end
+
+    # def bank_account
+    #     @bank_account
+    # end
+
+    def bank_account=(value)
+        @bank_account=(value)
+    end
+
+    # def happiness
+    #     @happiness
+    # end
+
+    def happiness=(num)
+    @happiness =  if num >10
+                        10
+                    elsif num < 1
+                        1
+                    else
+                        num
+    end
+
+    # def hygeine
+    #     @hygeine
+    # end
+
+    def hygeine=(num)
+       @hygeine =  if num > 10 
+                        10
+                    elsif num < 1
+                        1
+                    else
+                        num
+    end
+
+#end of class
+end
+
+# binding.pry
